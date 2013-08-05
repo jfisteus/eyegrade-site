cecfb833c85917196dc23045d62e22ffce2f3c37
Jesus Arias Fisteus <jaf@it.uc3m.es>
2013-08-05 14:20:04 +0200

    Add Google Analytics to the blog.

--- conf.py
+++ conf.py
@@ -1,4 +1,3 @@
-
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 import time
@@ -447,6 +446,18 @@ RSS_TEASERS = False
 # Google analytics or whatever else you use. Added to the bottom of <body>
 # in the default template (base.tmpl).
 # ANALYTICS = ""
+ANALYTICS = """
+<script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-42957388-1', 'eyegrade.org');
+  ga('send', 'pageview');
+
+</script>
+"""
 
 # The possibility to extract metadata from the filename by using a
 # regular expression.
